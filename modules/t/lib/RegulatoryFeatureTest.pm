ad40674d95a76cee84e4bd3cca1345ac418cd722
Ilias Lavidas <ilavidas@ebi.ac.uk>
2019-06-18 09:30:36 +0100

    bug fix

--- modules/t/lib/RegulatoryFeatureTest.pm
+++ modules/t/lib/RegulatoryFeatureTest.pm
@@ -291,7 +291,7 @@ sub get_epigenomes_by_activity :Test(2) {
     my $invalid_activity = $self->{parameters}->{invalid_activity};
 
     throws_ok {
-        $self->{fetched}->[0]->get_epigenomes_by_activity($invalid_activity)
+        $self->{fetched}->[0]->get_epigenomes_by_activity($invalid_activity);
     }
         qr/Please pass a valid activity to this method/,
         "... and exception is thrown when the passed activity is invalid";
